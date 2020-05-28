a50a0e926c2ebc173d53bf783ffd7253a990ad08
amreshk005 <amreshk005@gmail.com>
2020-05-28 21:30:32 +0530

    Updates

---
+++ precache-manifest.b6be004a88efac6ab67d489aeb1bb73b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32bdbf8ec40a01623e3a788487849c90",
+    "url": "/amreshk005/ppoper-clone/index.html"
+  },
+  {
+    "revision": "f2fd55e80745f58076ea",
+    "url": "/amreshk005/ppoper-clone/static/css/main.470fa8c1.chunk.css"
+  },
+  {
+    "revision": "330a09d5198bab437b1c",
+    "url": "/amreshk005/ppoper-clone/static/js/2.c10f686e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/amreshk005/ppoper-clone/static/js/2.c10f686e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2fd55e80745f58076ea",
+    "url": "/amreshk005/ppoper-clone/static/js/main.00fcbc55.chunk.js"
+  },
+  {
+    "revision": "35d6033cb6ec9b099c66",
+    "url": "/amreshk005/ppoper-clone/static/js/runtime-main.33aa6c94.js"
+  }
+]);
